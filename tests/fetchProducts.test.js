3e32aeeeb42d0b14c3afd181b8da801307623b34
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-06-11 17:48:02 -0300

    Adciona requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,9 +9,26 @@ describe('1 - Teste a função fetchProducts', () => {
     expect(typeof fetchProducts === 'function').toEqual(true);
   })
 
-  it('Testa se "fetch" é chamado quando fetchProducts receb um argumento', async () => {
-    expect.assertions(1);
-    await expect(fetchProducts('computador')).toBeCalled();
+  it('Testa se "fetch" é chamado quando fetchProducts recebe um argumento', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
   })
- 
+
+  it('Testa se ENDPOINT está correto', async () =>{
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  })
+
+  it('Testa se ao chamar a função fetchProducts sem argumento retorna mensagem de erro', async () =>{
+    /* await expect(fetchProducts()).rejects.toMatch('You must provide an url') */
+    try {
+      const qualquer = await fetchProducts()
+      
+    } catch (error) {
+      expect(error).toBe('You must provide an url')
+      
+    }
+  })
 });
+
+/* Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: 'You must provide an url'. */
